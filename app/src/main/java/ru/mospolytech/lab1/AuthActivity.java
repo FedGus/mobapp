8862da80bc6dd045f323ed00b36ede1b1a259625
Fedor Gusev <gusevfedya@mail.ru>
2020-12-04 17:54:47 +0300

    adding screens for the first time

---
+++ app/src/main/java/ru/mospolytech/lab1/AuthActivity.java
@@ -0,0 +1,14 @@
+package ru.mospolytech.lab1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AuthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
