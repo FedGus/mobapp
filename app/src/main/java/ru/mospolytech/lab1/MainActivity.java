42f6e8ea21c6be65209b47c450a09aed01794cbe
Fedor Gusev <gusevfedya@mail.ru>
2020-12-24 18:16:49 +0300

    create a side menu

--- app/src/main/java/ru/mospolytech/lab1/MainActivity.java
+++ app/src/main/java/ru/mospolytech/lab1/MainActivity.java
@@ -1,131 +1,65 @@
 package ru.mospolytech.lab1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.Menu;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import java.util.ArrayList;
-import java.util.List;
+import ru.mospolytech.lab1.R;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-    private static final String TAG = "A";
-    EditText textSearch;
-    RecyclerView recyclerView;
-    ListAdapter adapter;
-    List<ProductDetail> list;
-    ApiInterface api;
-    private CompositeDisposable disposables;
-    private GoogleMap mMap;
-    private UiSettings mUiSettings;
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        list = new ArrayList<>();
-        adapter = new ListAdapter(this, list);
-        recyclerView = findViewById(R.id.list);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        textSearch = findViewById(R.id.textSearch);
-        api = ApiConfiguration.getApi();
-        disposables = new CompositeDisposable();
-        this.onClick(this.recyclerView);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        disposables.add(api.productlist(textSearch.getText().toString())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe((productsList) -> {
-
-                    findViewById(R.id.progressBar).setVisibility(View.GONE);
-                    findViewById(R.id.list).setVisibility(View.VISIBLE);
-                    list.clear();
-                    list.addAll(productsList.all);
-                    adapter.notifyDataSetChanged();
-                    for (int lol = 0 ; lol < list.size(); lol++) {
-                        LatLng pin = new LatLng(Double.parseDouble(list.get(lol).latitude), Double.parseDouble(list.get(lol).longitude));
-                        mMap.addMarker(new MarkerOptions()
-                                .position(pin)
-                                .title(list.get(lol).title));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(pin));
-                    }
-                }, (error) -> {
-                    Toast.makeText(this, "При поиске возникла ошибка:\n" + error.getMessage(),
-                            Toast.LENGTH_LONG).show();
-                    findViewById(R.id.progressBar).setVisibility(View.GONE);
-                    findViewById(R.id.list).setVisibility(View.VISIBLE);
-
-                }));
-
-
-
-    }
-
-    public void onClick(View view){
-        if (textSearch.getText().toString().isEmpty()){
-            list.clear();
-        } else {
-            disposables.add(api.productlist(textSearch.getText().toString())
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe((productsList) -> {
-                        findViewById(R.id.progressBar).setVisibility(View.GONE);
-                        findViewById(R.id.list).setVisibility(View.VISIBLE);
-                        list.clear();
-                        list.addAll(productsList.all);
-                        adapter.notifyDataSetChanged();
-                    }, (error) -> {
-                        Toast.makeText(this, "При поиске возникла ошибка:\n" + error.getMessage(),
-                                Toast.LENGTH_LONG).show();
-                        findViewById(R.id.progressBar).setVisibility(View.GONE);
-                        findViewById(R.id.list).setVisibility(View.VISIBLE);
-
-                    }));
-            Toast.makeText(this, "Показаны результаты поиска: " + textSearch.getText().toString(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mUiSettings = mMap.getUiSettings();
-
-        // Keep the UI Settings state in sync with the checkboxes.
-        mUiSettings.setZoomControlsEnabled(true);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_gallery, R.id.nav_home, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (disposables.isDisposed()) {
-            disposables.dispose();
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
